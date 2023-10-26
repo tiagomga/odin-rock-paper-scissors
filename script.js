6eb3c1c20a34cc9f498823f130d5c80a03df3bbe
Tiago <109687852+tiagomga@users.noreply.github.com>
2023-10-26 01:20:29 +0100

    Create script.js and create logic for 'Rock Paper Scissors' game

---
+++ script.js
@@ -0,0 +1,74 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.localeCompare(computerSelection) === 0) {
+        return `${playerSelection} vs. ${computerSelection}. It's a tie!`;
+    } else if (playerSelection.localeCompare("rock") === 0 && computerSelection.localeCompare("paper") === 0) {
+        computerScore++;
+        return `${playerSelection} vs. ${computerSelection}. You lose this round!`;
+    } else if (playerSelection.localeCompare("rock") === 0 && computerSelection.localeCompare("scissors") === 0) {
+        playerScore++;
+        return `${playerSelection} vs. ${computerSelection}. You win this round!`;
+    } else if (playerSelection.localeCompare("paper") === 0 && computerSelection.localeCompare("rock") === 0) {
+        playerScore++;
+        return `${playerSelection} vs. ${computerSelection} . You win this round!`;
+    } else if (playerSelection.localeCompare("paper") === 0 && computerSelection.localeCompare("scissors") === 0) {
+        computerScore++;
+        return `${playerSelection} vs. ${computerSelection}. You lose this round!`;
+    } else if (playerSelection.localeCompare("scissors") === 0 && computerSelection.localeCompare("rock") === 0) {
+        computerScore++;
+        return `${playerSelection} vs. ${computerSelection}. You lose this round!`;
+    } else if (playerSelection.localeCompare("scissors") === 0 && computerSelection.localeCompare("paper") === 0) {
+        playerScore++;
+        return `${playerSelection} vs. ${computerSelection}. You win this round!`;
+    }
+}
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice === 0) {
+        choice = "rock";
+    } else if (choice === 1) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+
+    return choice;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+
+        while (playerSelection.localeCompare("rock") !== 0 &&
+                playerSelection.localeCompare("paper") !== 0 &&
+                playerSelection.localeCompare("scissors") !== 0) {
+            playerSelection = prompt("Invalid input! Rock, paper or scissors?").toLowerCase();
+        }
+
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations! You won!");
+    } else if (playerScore === computerScore) {
+        console.log("It's a tie!");
+    } else {
+        console.log("Boohoo! You lost!");
+    }
+}
+
+while (true) {
+    game();
+    answer = prompt("Repeat? (y/n)").toLowerCase();
+    if (answer.localeCompare("y") === 0) {
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+        break;
+    }
+}
